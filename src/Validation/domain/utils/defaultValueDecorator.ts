1293d257834e64f44c12eb8b772d9856128cc24a
ArtVolchara <mesmer_93@mail.ru>
2026-02-27 02:14:22 +0300

    defaultValueDecorator fixes

--- src/Validation/domain/utils/defaultValueDecorator.ts
+++ src/Validation/domain/utils/defaultValueDecorator.ts
@@ -5,32 +5,46 @@ import SuccessResult from '../../../_Root/domain/factories/SuccessResult';
 // Перегрузка с фабрикой значения по умолчанию на основе ошибки (более специфичная — первой)
 function defaultValueDecorator<
   const RuleOrValidator extends (value: any, ...args: any[]) => ISuccess | IError<string, any>,
-  const DefaultFactory extends (error: Extract<ReturnType<RuleOrValidator>, IError<string, any>>) => any,
+  const DefaultFactory extends (
+    error: Extract<ReturnType<RuleOrValidator>, IError<string, any>>
+  ) => Extract<ReturnType<RuleOrValidator>, ISuccess>['data'],
+  const IsEnabled extends boolean,
 >(
   ruleOrValidator: RuleOrValidator,
   defaultFactory: DefaultFactory,
-): (
-  value: Parameters<RuleOrValidator>[0],
-) => Extract<ReturnType<RuleOrValidator>, ISuccess> | ISuccess<ReturnType<DefaultFactory>>;
+  isEnabled?: IsEnabled,
+): IsEnabled extends true
+  ? (value: Parameters<RuleOrValidator>[0]) => Extract<ReturnType<RuleOrValidator>, ISuccess> | ISuccess<ReturnType<DefaultFactory>>
+  : RuleOrValidator;
 
 // Перегрузка со статическим значением по умолчанию
 function defaultValueDecorator<
   const RuleOrValidator extends (value: any, ...args: any[]) => ISuccess | IError<string, any>,
-  const DefaultValue,
+  const DefaultValue extends Extract<ReturnType<RuleOrValidator>, ISuccess>['data'],
+  const IsEnabled extends boolean,
 >(
   ruleOrValidator: RuleOrValidator,
   defaultValue: DefaultValue,
-): (
-  value: Parameters<RuleOrValidator>[0],
-) => Extract<ReturnType<RuleOrValidator>, ISuccess> | ISuccess<DefaultValue>;
+  isEnabled?: IsEnabled,
+): IsEnabled extends true
+  ? (
+    value: Parameters<RuleOrValidator>[0],
+  ) => Extract<ReturnType<RuleOrValidator>, ISuccess> | ISuccess<DefaultValue>
+  : RuleOrValidator;
 
 function defaultValueDecorator<
   const RuleOrValidator extends (value: any, ...args: any[]) => ISuccess | IError<string, any>,
-  const DefaultValueOrFactory,
+  const DefaultValueOrFactory extends Extract<ReturnType<RuleOrValidator>, ISuccess>['data']
+  | ((error: Extract<ReturnType<RuleOrValidator>, IError<string, any>>) => Extract<ReturnType<RuleOrValidator>, ISuccess>['data']),
+  const IsEnabled extends boolean,
 >(
   ruleOrValidator: RuleOrValidator,
   defaultValueOrFactory: DefaultValueOrFactory,
+  isEnabled?: IsEnabled,
 ) {
+  if (!isEnabled) {
+    return ruleOrValidator;
+  }
   return (value: Parameters<RuleOrValidator>[0]) => {
     const result = ruleOrValidator(value);
     if (result.status === 'error') {
